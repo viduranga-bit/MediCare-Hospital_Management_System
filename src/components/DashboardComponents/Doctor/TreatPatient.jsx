cbf41c91d87dd80c910016ac38aa49d6e306afc5
Amith Viduranga <62999097+viduranga-bit@users.noreply.github.com>
2023-04-09 14:03:15 +0530

    treat patient component done

--- src/components/DashboardComponents/Doctor/TreatPatient.jsx
+++ src/components/DashboardComponents/Doctor/TreatPatient.jsx
@@ -60,18 +60,18 @@ export default function TreatPatient() {
 
   const loadLabReport = async () => {
     const result = await axios
-      .get(`http://localhost:8080/api/v1/labReport`)
-      .then((res) => {
-        setlabReport(res.data);
-      });
+        .get(`http://localhost:8080/api/v1/labReport`)
+        .then((res) => {
+          setlabReport(res.data);
+        });
   };
   const loadPatients = async () => {
     const result = await axios
-      .get(`http://localhost:8080/api/v1/patients/${patientId}`)
-      .then((res) => {
-        setPatientData(res.data);
-        setIsRequestTest(res.data.isRequestTest);
-      });
+        .get(`http://localhost:8080/api/v1/patients/${patientId}`)
+        .then((res) => {
+          setPatientData(res.data);
+          setIsRequestTest(res.data.isRequestTest);
+        });
   };
 
   const [prescription, setPrescription] = useState({
@@ -88,34 +88,34 @@ export default function TreatPatient() {
   const onSubmit = async (e) => {
     e.preventDefault();
     await axios
-      .post("http://localhost:8080/api/v1/prescription", prescription)
-      .then((r) => {
-        if (r.status === 200) {
-          toast.success("Patient Treatment is Successfull", {
-            position: "top-right",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
+        .post("http://localhost:8080/api/v1/prescription", prescription)
+        .then((r) => {
+          if (r.status === 200) {
+            toast.success("Patient Treatment is Successfull", {
+              position: "top-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
 
-          navigate(-1);
-        } else {
-          toast.error("Patient Treatment is Unsuccessfull", {
-            position: "top-right",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
-        }
-      });
+            navigate(-1);
+          } else {
+            toast.error("Patient Treatment is Unsuccessfull", {
+              position: "top-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          }
+        });
 
     // const res = await axios.patch(
     //   `http://localhost:8080/api/v1/patients/${patientId}`,
@@ -125,7 +125,7 @@ export default function TreatPatient() {
   };
 
   const filteredReports = labReportData.filter(
-    (report) => report.patient_id == patientId
+      (report) => report.patient_id == patientId
   );
 
   const pdfBinaryData = filteredReports[0]?.labReport_Id;
@@ -139,207 +139,206 @@ export default function TreatPatient() {
   };
 
   return (
-    <div>
-      <div className="border shadow p-5 md:m-12 md:mt-4 md:p-10 bg-white rounded-3xl">
-        <Grid container spacing={2}>
-          <Grid item xs={10}>
-            <Header title="Treat Patient - " value={patientData?.patientName} />
-            <p className="ml-1  text-1xl align Right font-extrabold tracking-tight text-slate-700">
-              Age : {patientData?.age} | Gender : {patientData?.gender}
-            </p>
-          </Grid>
-          <Grid item xs={2}>
-            <p className="mt-3  text-1xl align Right font-extrabold tracking-tight text-slate-600">
-              Patient ID : {patientData?.patientId}
-            </p>
-          </Grid>
-          <Grid item xs={12}>
-            <div>
-              <form className="m-3  p-2 md:p-10 " onSubmit={(e) => onSubmit(e)}>
-                <Box
-                  sx={{
-                    "& .MuiTextField-root": { mt: 2 },
-                  }}
-                >
-                  <p className=" mb-2 align Right font-extrabold  text-slate-700">
-                    CAPTURE PATIENT'S SYMPTOMS
-                  </p>
-
-                  <div>
-                    <FormControl
+      <div>
+        <div className="border shadow p-5 md:m-12 md:mt-4 md:p-10 bg-white rounded-3xl">
+          <Grid container spacing={2}>
+            <Grid item xs={10}>
+              <Header title="Treat Patient - " value={patientData?.patientName} />
+              <p className="ml-1  text-1xl align Right font-extrabold tracking-tight text-slate-700">
+                Age : {patientData?.age} | Gender : {patientData?.gender}
+              </p>
+            </Grid>
+            <Grid item xs={2}>
+              <p className="mt-3  text-1xl align Right font-extrabold tracking-tight text-slate-600">
+                Patient ID : {patientData?.patientId}
+              </p>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <form className="m-3  p-2 md:p-10 " onSubmit={(e) => onSubmit(e)}>
+                  <Box
                       sx={{
-                        mt: 2,
+                        "& .MuiTextField-root": { mt: 2 },
                       }}
-                    >
-                      <Textarea
-                        value={symptoms}
-                        name="symptoms"
-                        onChange={(e) => onInputChange(e)}
-                        placeholder="Enter Patient's Symptoms one by one Here....."
-                        minRows={5}
-                      />
-                    </FormControl>
-                    <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
-                      PATIENT'S CASE HISTORY
+                  >
+                    <p className=" mb-2 align Right font-extrabold  text-slate-700">
+                      CAPTURE PATIENT'S SYMPTOMS
                     </p>
-                    <FormControl
-                      sx={{
-                        mt: 2,
-                      }}
-                    >
-                      <Textarea
-                        value={case_history}
-                        name="case_history"
-                        onChange={(e) => onInputChange(e)}
-                        placeholder="Enter Patient's case history Here....."
-                        minRows={5}
-                      />
-                    </FormControl>
-                  </div>
 
-                  <p className=" mt-7 mb-4 align Right font-extrabold  text-slate-700">
-                    SEND PATIENT TO LABROTARY
-                  </p>
-                  <Grid container spacing={2}>
-                    <Grid
-                      item
-                      alignItems="center"
-                      justifyContent="center"
-                      xs={12}
-                    >
-                      <Item>
-                        <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
-                          {isRequestTest
-                            ? "Your Lab report Request has been Sent Successfully..."
-                            : "Send the Patient to labrotary by clicking following button"}
-                        </p>
-                        <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
-                          {isRequestTest ? "Please Wait For the Result" : ""}
-                        </p>
-
-                        {pdfBinaryDatacontent === null && isRequestTest ? (
-                          <div className="flex gap-10 m-4 flex-wrap justify-center">
-                            <HashLoader color={currentColor} />
-                          </div>
-                        ) : (
-                          <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
-                            <div>
-                              <Button
-                                onClick={handleOpenPdf}
-                                variant="contained"
-                              >
-                                Show Result
-                              </Button>
-                            </div>
-                          </p>
-                        )}
-
-                        {!isRequestTest ? (
-                          <RoundedButton
-                            onClick={handleClickOpen}
-                            label="Send Request"
-                          />
-                        ) : (
-                          ""
-                        )}
-
-                        {/* {pdfUrl && (
-                          <iframe
-                            src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
-                            width="600"
-                            height="800"
-                            frameborder="0"
-                          ></iframe>
-                        )} */}
-                      </Item>
-                      <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
-                        ADD MEDICATIONS
-                      </p>
+                    <div>
                       <FormControl
-                        sx={{
-                          mt: 2,
-                        }}
+                          sx={{
+                            mt: 2,
+                          }}
                       >
                         <Textarea
-                          value={medication}
-                          name="medication"
-                          onChange={(e) => onInputChange(e)}
-                          placeholder="Enter Patient's case history Here....."
-                          minRows={5}
+                            value={symptoms}
+                            name="symptoms"
+                            onChange={(e) => onInputChange(e)}
+                            placeholder="Enter Patient's Symptoms one by one Here....."
+                            minRows={5}
                         />
                       </FormControl>
                       <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
-                        SPECIAL NOTES OR DESCIPTION
+                        PATIENT'S CASE HISTORY
                       </p>
                       <FormControl
-                        sx={{
-                          mt: 2,
-                        }}
+                          sx={{
+                            mt: 2,
+                          }}
                       >
                         <Textarea
-                          value={description}
-                          name="description"
-                          onChange={(e) => onInputChange(e)}
-                          placeholder="Add Sepcial Notes  Here....."
-                          minRows={5}
+                            value={case_history}
+                            name="case_history"
+                            onChange={(e) => onInputChange(e)}
+                            placeholder="Enter Patient's case history Here....."
+                            minRows={5}
                         />
                       </FormControl>
+                    </div>
 
-                      <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
-                        IS THIS PATIENT NEED TO BE ADMITTED ?
-                      </p>
-                      <FormControl
-                        sx={{
-                          m: 3,
-                        }}
+                    <p className=" mt-7 mb-4 align Right font-extrabold  text-slate-700">
+                      SEND PATIENT TO LABROTARY
+                    </p>
+                    <Grid container spacing={2}>
+                      <Grid
+                          item
+                          alignItems="center"
+                          justifyContent="center"
+                          xs={12}
                       >
-                        <RadioGroup
-                          defaultValue="female"
-                          row
-                          aria-labelledby="demo-row-radio-buttons-group-label"
-                          name="row-radio-buttons-group"
+                        <Item>
+                          {pdfBinaryDatacontent===null ?
+                              <div>
+                                <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
+                                  {isRequestTest
+                                      ? 'Your Lab report Request has been Sent Successfully...'
+                                      : 'Send the Patient to labrotary by clicking following button'}
+                                </p>
+                                <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
+                                  {isRequestTest ? (
+                                      'Please Wait For the Result'
+
+                                  ) : (
+                                      <RoundedButton
+                                          onClick={handleClickOpen}
+                                          label="Send Request"
+                                      />
+                                  )}
+                                </p>
+
+                                {isRequestTest ? (
+                                    <div className="flex gap-10 m-4 flex-wrap justify-center">
+                                      <HashLoader color={currentColor} />
+                                    </div>
+                                ) : ' '}
+                              </div>:
+                              <div>
+                                <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
+
+                                  Your Requested Lab report Result has been available...
+
+
+
+                                </p>
+                                <p className="mt-3 mb-4  text-1xl align Right font-extrabold tracking-tight text-slate-600">
+
+
+
+                                  <Button onClick = {handleOpenPdf} variant="contained">Show Result</Button>
+
+                                </p>
+                              </div>
+                          }
+                        </Item>
+                        <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
+                          ADD MEDICATIONS
+                        </p>
+                        <FormControl
+                            sx={{
+                              mt: 2,
+                            }}
                         >
-                          <FormControlLabel
+                          <Textarea
+                              value={medication}
+                              name="medication"
+                              onChange={(e) => onInputChange(e)}
+                              placeholder="Enter Patient's case history Here....."
+                              minRows={5}
+                          />
+                        </FormControl>
+                        <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
+                          SPECIAL NOTES OR DESCIPTION
+                        </p>
+                        <FormControl
+                            sx={{
+                              mt: 2,
+                            }}
+                        >
+                          <Textarea
+                              value={description}
+                              name="description"
+                              onChange={(e) => onInputChange(e)}
+                              placeholder="Add Sepcial Notes  Here....."
+                              minRows={5}
+                          />
+                        </FormControl>
+
+                        <p className=" mb-2 mt-8 align Right font-extrabold  text-slate-700">
+                          IS THIS PATIENT NEED TO BE ADMITTED ?
+                        </p>
+                        <FormControl
                             sx={{
                               m: 3,
-                              mr: 4,
                             }}
-                            value="INPATIENT"
-                            control={<Radio />}
-                            label="Yes"
-                          />
-                          <FormControlLabel
-                            value="OUTPATIENT"
-                            control={<Radio />}
-                            label="No"
-                          />
-                        </RadioGroup>
-                      </FormControl>
+                        >
+                          <RadioGroup
+                              defaultValue="female"
+                              row
+                              aria-labelledby="demo-row-radio-buttons-group-label"
+                              name="row-radio-buttons-group"
+                          >
+                            <FormControlLabel
+                                sx={{
+                                  m: 3,
+                                  mr: 4,
+                                }}
+                                value="INPATIENT"
+                                control={<Radio />}
+                                label="Yes"
+                            />
+                            <FormControlLabel
+                                value="OUTPATIENT"
+                                control={<Radio />}
+                                label="No"
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </Box>
-                <Button
-                  sx={{ alignItems: "center", m: 1, width: "22ch" }}
-                  variant="contained"
-                  size="medium"
-                  type="submit"
-                >
-                  Submit
-                </Button>
-              </form>
-            </div>
+                  </Box>
+                  <Button
+                      sx={{ alignItems: "center", m: 1, width: "22ch" }}
+                      variant="contained"
+                      size="medium"
+                      type="submit"
+                  >
+                    Submit
+                  </Button>
+                </form>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
+        </div>
 
-      <ToastContainer />
-      <LabrotaryRequestPopup
-        pid={patientId}
-        doctorId={doctorId}
-        setOpen={setOpen}
-        open={open}
-        setIsRequestTest={setIsRequestTest}
-      />
-    </div>
+        <ToastContainer />
+        <LabrotaryRequestPopup
+            pid={patientId}
+            doctorId={doctorId}
+            setOpen={setOpen}
+            open={open}
+            setIsRequestTest={setIsRequestTest}
+        />
+      </div>
   );
 }
